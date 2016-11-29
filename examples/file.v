6cfd6326f8a92db8bffb8f17a217436da021207a
SRIJIT DUTTA <srijit@cse.iitb.ac.in>
2016-11-29 22:31:41 +0530

    examples and executable added

---
+++ examples/file.v
@@ -0,0 +1,11 @@
+// Benchmark "file" written by ABC on Tue Nov 29 19:02:41 2016
+
+module file ( 
+    a, b, c, d,
+    f3  );
+  input  a, b, c, d;
+  output f3;
+  assign f3 = a ^ ~b;
+endmodule
+
+
